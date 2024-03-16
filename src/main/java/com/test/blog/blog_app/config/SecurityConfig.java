3d68e0bedbacfae136d367c90f7c037a981741a9
job.allan <jobalan.odhiambo@tangazoletu.com>
2024-03-16 17:48:02 +0300

    - Added configuration & EnableMethodSecurity annotations to SecurityConfig class

--- src/main/java/com/test/blog/blog_app/config/SecurityConfig.java
+++ src/main/java/com/test/blog/blog_app/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.test.blog.blog_app.config;
 import com.test.blog.blog_app.security.JwtAuthenticationEntryPoint;
 import com.test.blog.blog_app.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     private UserDetailsService userDetailsService;
 

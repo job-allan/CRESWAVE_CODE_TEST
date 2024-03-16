c9256f8ff7c7468e0d4d48ffc44651900193bb29
job.allan <jobalan.odhiambo@tangazoletu.com>
2024-03-16 14:18:05 +0300

    - Implemented Post rest apis

---
+++ src/main/java/com/test/blog/blog_app/payload/PostDto.java
@@ -0,0 +1,11 @@
+package com.test.blog.blog_app.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
---
+++ src/main/java/com/test/blog/blog_app/service/PostService.java
@@ -0,0 +1,17 @@
+package com.test.blog.blog_app.service;
+
+import com.test.blog.blog_app.payload.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}

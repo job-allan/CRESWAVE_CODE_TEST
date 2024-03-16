68ed98c9976a273458047afff716aba2c243f5ef
job.allan <jobalan.odhiambo@tangazoletu.com>
2024-03-16 14:58:14 +0300

    - Implemented comments apis

---
+++ src/main/java/com/test/blog/blog_app/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.test.blog.blog_app.repository;
+
+import com.test.blog.blog_app.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(long postId);
+}
